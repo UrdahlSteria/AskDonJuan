3989b06d00bbf32dd1c4cf9fe495af4dbdffcd3b
UrdahlSteria <ole.urdal@soprasteria.com>
2016-05-03 23:22:45 +0200

    Some sort of beginning
    - Created the QueryModel for setting the attributes of a "break-up"
    query.
    - Started documentation
    - Get your head into rule engines bitches!

---
+++ src/AskDonJuan/Models/QueryModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskDonJuan.Models
+{
+    public class QueryModel
+    {
+        //Should InRelationship be unknown as well?
+        [Display(Name = "Are you in a relationship?")]
+        public bool InRelationship { get; set; }
+
+        [Display(Name = "My Age")]
+        [Range(16, 99)]
+        [Required(ErrorMessage = "Please enter your age.")]
+        public int UserAge { get; set; }
+
+        [Display(Name = "Partner Age")]
+        [Range(16,99)]
+        public int OtherAge { get; set; }
+
+        [Display(Name = "Relationship Length")]
+        [DataType(DataType.Duration)]        // Uncertain weather this is the correct DataType for TimeSpan
+        //[DisplayFormat(DataFormatString = "{0:MM/dd/yy}", ApplyFormatInEditMode = true)]
+        public TimeSpan DatingLength { get; set; }
+
+        //[Display(Name = "How mean are you?")]
+        //[Required(ErrorMessage="Please enter how kind/mean you want to be")]
+        //[EnumDataType()]
+        public enum MeanessEnum
+        {
+            Kind,
+            Normal,
+            Mean
+        };
+
+    }
+}
